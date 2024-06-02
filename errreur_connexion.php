ba036d02ad790389fd58d15a028c720ffddfb4d6
wandrille99 <133336590+wandrille99@users.noreply.github.com>
2024-06-02 17:10:30 +0200

    Create errreur_connexion.php

---
+++ errreur_connexion.php
@@ -0,0 +1,15 @@
+<?php
+include 'header.php';
+?>
+
+<main>
+    <section id="erreur-connexion">
+        <div class="container">
+            <h2>Erreur de Connexion</h2>
+            <p>Vous devez être connecté pour accéder à cette page. Veuillez vous connecter et réessayer.</p>
+            <a href="acceuil.php" class="btn">Retour à l'accueil</a>
+        </div>
+    </section>
+</main>
+
+<?php include 'footer.php'; ?>
