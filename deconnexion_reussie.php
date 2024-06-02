3e901eba144bf8f404977a0bf3cb64ea7f9fa3c8
filou94 <132278817+filou94@users.noreply.github.com>
2024-06-02 13:11:04 +0200

    Create deconnexion_reussie.php

---
+++ deconnexion_reussie.php
@@ -0,0 +1,13 @@
+<?php include 'header.php'; ?>
+
+<main>
+    <section id="success">
+        <div class="container">
+            <h2>Vous êtes bien déconnecté</h2>
+            <p>À bientôt sur notre site !</p>
+            <a href="acceuil.php" class="btn">Retour à l'accueil</a>
+        </div>
+    </section>
+</main>
+
+<?php include 'footer.php'; ?>
